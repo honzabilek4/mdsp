92d8453b9db892a446baf2f91363a2835d452cd3
Bilek Jan <jan.bilek@insa-rennes.fr>
2016-01-28 10:42:05 +0100

    syntax fixesin dominantVec

--- include/dominantVec.h
+++ include/dominantVec.h
@@ -3,10 +3,10 @@
 
 #include "preesm.h"
 
-typedef struct ResCon{
+typedef struct {
 	   int count;
 	   int index;
-};
+} ResCon;
 void dominantVec(int width, int height, int blockSize ,IN int *input_x , int *input_y ,OUT int *output);
 ResCon getCandidate(int start , int *inputx, int *inputy, int numSubFrames) ;
 
--- src/dominantVec.c
+++ src/dominantVec.c
@@ -5,38 +5,39 @@
 
 void dominantVec(int width, int height, int blockSize ,IN int *input_x , int *input_y ,OUT int *output) {
 
-    int numSubFrames = (width + blockSize - width%blockSize)/blockSize)*
-                       (height + blockSize - height%blockSize)/blockSize;
+    int numSubFrames = ((width + blockSize - width%blockSize)/blockSize)*
+                       ((height + blockSize - height%blockSize)/blockSize);
 
     int startVal = 0 ;
-    ResCon result = getCandiate (startVal ,inputx ,inputy , numSubFrames);
+    ResCon result = getCandidate (startVal ,input_x ,input_y , numSubFrames);
     int numbofLoops = 1 ;
     int count = result.count ;
     int index = result.index ;
 
     while (numbofLoops < numSubFrames || count < numSubFrames/3){
         startVal++;
-        result = getCandiate(startVal ,inputx ,inputy , numSubFrames);
+        result = getCandidate(startVal ,input_x ,input_y , numSubFrames);
         numbofLoops++ ;
         if(count < result.count){
             index = result.index ;
             count = result.count ;
         }
     }
-	memset(output, *(inputx+result.index) , 1);
+	memset(output, *(input_x+result.index) , 1);
 	output ++;
-	memset(output, *(inputy+result.index) , 1);
+	memset(output, *(input_y+result.index) , 1);
     ///return index;
 }
 
 
 ResCon getCandidate(int start , int *inputx, int *inputy, int numSubFrames){
+    int i;
     int maj_index = start ;
     float borderx = *(inputx + start)/16 ;
 	float bordery = *(inputy + start)/16 ;
     int count, count1 = 1 ;
     ResCon result ;
-    for(i = start+1 ; i < numSubFram ; i++){
+    for(i = start+1 ; i < numSubFrames ; i++){
         if(*(inputx + i) <= *(inputx + maj_index) + borderx &&
         *(inputx +maj_index) - borderx <= *(inputx+i) &&
 		*(inputy + i) <= *(inputy + maj_index) + bordery &&
@@ -50,11 +51,11 @@ ResCon getCandidate(int start , int *inputx, int *inputy, int numSubFrames){
             maj_index = i ;
             count = 1 ;
             count1 = 1 ;
-            border= *(input + maj_index)/16;
+            borderx= *(inputx + maj_index)/16;
+            bordery= *(inputy + maj_index)/16;
         }
     }
     result.index = maj_index ;
     result.count = count1 ;
     return result ;
-}
 }
